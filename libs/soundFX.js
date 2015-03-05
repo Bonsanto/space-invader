d390352e59d7e6cd7e68d12ffcf6f29fa12ef7b9
Bonsanto <xbonsx@gmail.com>
2015-03-05 18:29:26 -0430

    - Substantial reduction of the size of all sounds and pictures.

--- libs/soundFX.js
+++ libs/soundFX.js
@@ -1,39 +1,39 @@
 var menuSoundtrack = new Audio();
-menuSoundtrack.src = "libs/soundEffects/menu-x.mp3";
+menuSoundtrack.src = "libs/soundEffects/menu.mp3";
 menuSoundtrack.loop = true;
-menuSoundtrack.volume = 0.2;
+menuSoundtrack.volume = 0.5;
 
 var gameSoundtrack = new Audio();
-gameSoundtrack.src = "libs/soundEffects/soundtrack2.mp3";
+gameSoundtrack.src = "libs/soundEffects/game.mp3";
 gameSoundtrack.loop = true;
-gameSoundtrack.volume = 0.2;
+gameSoundtrack.volume = 0.5;
 
 var cityhit = new Audio();
 cityhit.src = "libs/soundEffects/city1.mp3";
-cityhit.volume = 0.2;
+cityhit.volume = 0.05;
 
 var alienshot = new Audio();
 alienshot.src = "libs/soundEffects/alienshot1.mp3";
-alienshot.volume = 0.2;
+alienshot.volume = 0.05;
 
 var alienhit = new Audio();
 alienhit.src = "libs/soundEffects/alienhit1.mp3";
-alienhit.volume = 0.2;
+alienhit.volume = 0.05;
 
 var tankshot = new Audio();
 tankshot.src = "libs/soundEffects/tankShot2.wav";
-tankshot.volume = 0.2;
+tankshot.volume = 0.05;
 
 var tankhit = new Audio();
 tankhit.src = "libs/soundEffects/tankhit1.mp3";
-tankhit.volume = 0.2;
+tankhit.volume = 0.05;
 
 var victorySound = new Audio();
-victorySound.src = "libs/soundEffects/victorySound.mp3";
-victorySound.volume = 0.2;
+victorySound.src = "libs/soundEffects/victory.mp3";
+victorySound.volume = 0.5;
 victorySound.loop = true;
 
 var defeatSound = new Audio();
-defeatSound.src = "libs/soundEffects/defeatSound.mp3";
-defeatSound.volume = 0.2;
+defeatSound.src = "libs/soundEffects/defeat.mp3";
+defeatSound.volume = 0.5;
 defeatSound.loop = true;
